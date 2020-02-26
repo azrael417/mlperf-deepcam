237a1569da99fdcd24096d2504de8b4e1641879a
Awesom E. Dev <devnull@nvidia.com>
2020-02-26 09:38:41 -0800

    updated docker build

--- build_docker.sh
+++ build_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #nvidia-docker build -t tkurth/pytorch-bias_gan:latest .
-nvidia-docker build -t tkurth/pytorch-deepcam_mlperf:latest .
-#docker push registry.services.nersc.gov/tkurth/pytorch-bias_gan:latest
+nvidia-docker build -t gitlab-master.nvidia.com/tkurth/pytorch-deepcam_mlperf:latest .
+docker push gitlab-master.nvidia.com/tkurth/pytorch-deepcam_mlperf:latest
 
 #run docker test
 #docker run --device=/dev/nvidia-fs0 --workdir "/opt/pytorch/numpy_reader/scripts" -it tkurth/pytorch-numpy_reader:latest ./reader_test.sh
