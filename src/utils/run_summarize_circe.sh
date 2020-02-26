cfb0f2db0a8322031b96c9ca7878c82aa8dba40e
Thorsten Kurth <tkurth@nvidia.com>
2020-02-26 15:02:00 -0800

    fixed bash submission script

--- src/utils/run_summarize_circe.sh
+++ src/utils/run_summarize_circe.sh
@@ -2,12 +2,12 @@
 #SBATCH -A hpc
 #SBATCH -J summarize_cam5
 #SBATCH -t 01:00:00
-#SBATCH --mpi=pmix
-#SBATCH --container-workdir=/opt/utils
-#SBATCH --container-mounts=/gpfs/fs1/tkurth/cam5_dataset/All-Hist:/data
-#SBATCH --container-image=gitlab-master.nvidia.com/tkurth/mlperf-deepcam:debug
 
 rankspernode=32
 totalranks=$(( ${SLURM_NNODES} * ${rankspernode} ))
 
-srun -N ${SLURM_NNODES} -n ${totalranks} python summarize_data.py
+srun --mpi=pmix -N ${SLURM_NNODES} -n ${totalranks} \
+     --container-workdir=/opt/utils \
+     --container-mounts=/gpfs/fs1/tkurth/cam5_dataset/All-Hist:/data \
+     --container-image=gitlab-master.nvidia.com/tkurth/mlperf-deepcam:debug \
+     python summarize_data.py
