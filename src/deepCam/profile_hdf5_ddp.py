8310c829b3fe8d5247824d4a7f8ce04001e0fc67
Awesom E. Dev <devnull@nvidia.com>
2020-03-28 12:50:55 -0700

    added a profiling version

---
+++ src/deepCam/profile_hdf5_ddp.py
@@ -0,0 +1,259 @@
+# Basics
+import os
+import numpy as np
+import argparse as ap
+import datetime as dt
+import subprocess as sp
+
+# Torch
+import torch
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+# Custom
+from utils import utils
+from utils import losses
+from utils import parsing_helpers as ph
+from data import cam_hdf5_dataset as cam
+from architecture import deeplab_xception
+
+#DDP
+import torch.distributed as dist
+try:
+    from apex import amp
+    import apex.optimizers as aoptim
+    from apex.parallel import DistributedDataParallel as DDP
+    have_apex = True
+except:
+    from torch.nn.parallel.distributed import DistributedDataParallel as DDP
+    have_apex = False
+
+#comm wrapper
+from utils import comm
+
+#we need pycuda
+import pycuda.autoinit
+import pycuda as pyc
+
+#dict helper for argparse
+class StoreDictKeyPair(ap.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        my_dict = {}
+        for kv in values.split(","):
+            k,v = kv.split("=")
+            my_dict[k] = v
+        setattr(namespace, self.dest, my_dict)
+
+
+def printr(msg, rank=0):
+    if comm.get_rank() == rank:
+        print(msg)
+
+
+class Profile():
+    
+    def __init__(self, params, flag, steps):
+        self.flag == flag
+        self.tflag = params.profile
+        self.num_warmup_steps = params.num_warmup_steps
+        self.steps = steps
+        self.profiler_started = False
+
+    def __enter__(self):
+        if (self.flag == self.tflag) and (self.num_warmup_steps >= self.steps):
+            pyc.driver.start_profiler()
+            self.profiler_started = True
+
+    def __exit__(self, *args):
+        if self.profiler_started:
+            pyc.driver.stop_profiler()
+            self.profiler_started = False
+
+
+#main function
+def main(pargs):
+
+    #init distributed training
+    comm.init(pargs.wireup_method)
+    comm_rank = comm.get_rank()
+    comm_local_rank = comm.get_local_rank()
+    comm_size = comm.get_size()
+    
+    #set seed
+    seed = 333
+    
+    # Some setup
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        printr("Using GPUs",0)
+        device = torch.device("cuda", comm_local_rank)
+        torch.cuda.manual_seed(seed)
+        #necessary for AMP to work
+        torch.cuda.set_device(device)
+    else:
+        printr("Using CPUs",0)
+        device = torch.device("cpu")
+
+    #set up directories
+    root_dir = os.path.join(pargs.data_dir_prefix)
+    output_dir = pargs.output_dir
+    plot_dir = os.path.join(output_dir, "plots")
+    if comm_rank == 0:
+        if not os.path.isdir(output_dir):
+            os.makedirs(output_dir)
+
+    # Define architecture
+    n_input_channels = len(pargs.channels)
+    n_output_channels = 3
+    net = deeplab_xception.DeepLabv3_plus(n_input = n_input_channels, 
+                                          n_classes = n_output_channels, 
+                                          os=16, pretrained=False, 
+                                          rank = comm_rank)
+    net.to(device)
+
+    #select loss
+    loss_pow = pargs.loss_weight_pow
+    #some magic numbers
+    class_weights = [0.986267818390377**loss_pow, 0.0004578708870701058**loss_pow, 0.01327431072255291**loss_pow]
+    fpw_1 = 2.61461122397522257612
+    fpw_2 = 1.71641974795896018744
+    criterion = losses.fp_loss
+
+    #select optimizer
+    optimizer = None
+    if pargs.optimizer == "Adam":
+        optimizer = optim.Adam(net.parameters(), lr = pargs.start_lr, eps = pargs.adam_eps, weight_decay = pargs.weight_decay)
+    elif pargs.optimizer == "AdamW":
+        optimizer = optim.AdamW(net.parameters(), lr = pargs.start_lr, eps = pargs.adam_eps, weight_decay = pargs.weight_decay)
+    elif have_apex and (pargs.optimizer == "LAMB"):
+        optimizer = aoptim.FusedLAMB(net.parameters(), lr = pargs.start_lr, eps = pargs.adam_eps, weight_decay = pargs.weight_decay)
+    else:
+        raise NotImplementedError("Error, optimizer {} not supported".format(pargs.optimizer))
+
+    if have_apex:
+        #wrap model and opt into amp
+        net, optimizer = amp.initialize(net, optimizer, opt_level = pargs.amp_opt_level)
+    
+    #make model distributed
+    net = DDP(net)
+
+    #select scheduler
+    if pargs.lr_schedule:
+        scheduler_after = ph.get_lr_schedule(pargs.start_lr, pargs.lr_schedule, optimizer, last_step = start_step)
+
+    if pargs.lr_warmup_steps > 0:
+        scheduler = GradualWarmupScheduler(optimizer, multiplier=pargs.lr_warmup_factor, total_epoch=pargs.lr_warmup_steps, after_scheduler=scheduler_after)
+    else:
+        scheduler = scheduler_after
+
+    # Set up the data feeder
+    # train
+    train_dir = os.path.join(root_dir, "train")
+    train_set = cam.CamDataset(train_dir, 
+                               statsfile = os.path.join(root_dir, 'stats.h5'),
+                               channels = pargs.channels,
+                               shuffle = True, 
+                               preprocess = True,
+                               comm_size = comm_size,
+                               comm_rank = comm_rank)
+    train_loader = DataLoader(train_set, pargs.local_batch_size, num_workers=min([pargs.max_inter_threads, pargs.local_batch_size]), drop_last=True)
+    
+    # validation: we only want to shuffle the set if we are cutting off validation after a certain number of steps
+    validation_dir = os.path.join(root_dir, "validation")
+    validation_set = cam.CamDataset(validation_dir, 
+                               statsfile = os.path.join(root_dir, 'stats.h5'),
+                               channels = pargs.channels,
+                               shuffle = (pargs.max_validation_steps is not None),
+                               preprocess = True,
+                               comm_size = comm_size,
+                               comm_rank = comm_rank)
+    validation_loader = DataLoader(validation_set, pargs.local_batch_size, num_workers=min([pargs.max_inter_threads, pargs.local_batch_size]), drop_last=True)
+
+        
+    printr('{:14.4f} REPORT: starting warmup'.format(dt.datetime.now().timestamp()), 0)
+    step = 0
+    current_lr = pargs.start_lr if not pargs.lr_schedule else scheduler.get_last_lr()[0]
+    net.train()
+    while True:
+        
+        #for inputs_raw, labels, source in train_loader:
+        for inputs, label, filename in train_loader:
+            
+            # Print status
+            if step == pargs.num_warmup_steps:
+                printr('{:14.4f} REPORT: starting profiling'.format(dt.datetime.now().timestamp()), 0)
+            
+            # Forward pass
+            with Profile(params, "Forward", step):
+                #send data to device
+                inputs = inputs.to(device)
+                label = label.to(device)
+                
+                # Compute output
+                outputs = net.forward(inputs)
+            
+                # Compute loss
+                loss = criterion(outputs, label, weight=class_weights, fpw_1=fpw_1, fpw_2=fpw_2)
+            
+            # allreduce for loss
+            loss_avg = loss.detach()
+            dist.reduce(loss_avg, dst=0, op=dist.ReduceOp.SUM)
+            
+            # Compute score
+            predictions = torch.max(outputs, 1)[1]
+            iou = utils.compute_score(predictions, label, device_id=device, num_classes=3)
+            iou_avg = iou.detach()
+            dist.reduce(iou_avg, dst=0, op=dist.ReduceOp.SUM)
+            
+            # Backprop
+            with Profile(params, "Backward", step):
+                optimizer.zero_grad()
+                if have_apex:
+                    with amp.scale_loss(loss, optimizer) as scaled_loss:
+                        scaled_loss.backward()
+                else:
+                    loss.backward()
+            
+            # weight update
+            with Profile(params, "Optimizer", step):
+                optimizer.step()
+
+            #step counter
+            step += 1
+
+            #are we done?
+            if step >= (pargs.num_warmup_steps + pargs.num_profile_steps):
+                break
+
+    printr('{:14.4f} REPORT: finishing profiling'.format(dt.datetime.now().timestamp()), 0)
+    
+
+if __name__ == "__main__":
+
+    #arguments
+    AP = ap.ArgumentParser()
+    AP.add_argument("--wireup_method", type=str, default="nccl-openmpi", choices=["nccl-openmpi", "nccl-slurm", "mpi"], help="Specify what is used for wiring up the ranks")
+    AP.add_argument("--run_tag", type=str, help="Unique run tag, to allow for better identification")
+    AP.add_argument("--output_dir", type=str, help="Directory used for storing output. Needs to read/writeable from rank 0")
+    AP.add_argument("--checkpoint", type=str, default=None, help="Checkpoint file to restart training from.")
+    AP.add_argument("--data_dir_prefix", type=str, default='/', help="prefix to data dir")
+    AP.add_argument("--max_inter_threads", type=int, default=1, help="Maximum number of concurrent readers")
+    AP.add_argument("--max_epochs", type=int, default=30, help="Maximum number of epochs to train")
+    AP.add_argument("--local_batch_size", type=int, default=1, help="Number of samples per local minibatch")
+    AP.add_argument("--num_warmup_steps", type=int, default=5, help="Number of warmup steps")
+    AP.add_argument("--num_profile_steps", type=int, default=1, help="Number of profiling steps")
+    AP.add_argument("--profile", type=str, default="Forward", choices=["Forward", "Backward", "Optimizer"], help="Flag which parts to profile")
+    AP.add_argument("--channels", type=int, nargs='+', default=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], help="Channels used in input")
+    AP.add_argument("--optimizer", type=str, default="Adam", choices=["Adam", "AdamW", "LAMB"], help="Optimizer to use (LAMB requires APEX support).")
+    AP.add_argument("--start_lr", type=float, default=1e-3, help="Start LR")
+    AP.add_argument("--adam_eps", type=float, default=1e-8, help="Adam Epsilon")
+    AP.add_argument("--weight_decay", type=float, default=1e-6, help="Weight decay")
+    AP.add_argument("--loss_weight_pow", type=float, default=-0.125, help="Decay factor to adjust the weights")
+    AP.add_argument("--lr_schedule", action=StoreDictKeyPair)
+    AP.add_argument("--amp_opt_level", type=str, default="O0", help="AMP optimization level")
+    pargs = AP.parse_args()
+
+    #run the stuff
+    main(pargs)
---
+++ src/deepCam/run_profiling_cori.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH -A dasrepo
+#SBATCH -J train_cam5
+#SBATCH -t 08:00:00
+#SBATCH -C knl
+#SBATCH -q regular
+#SBATCH -S 2
+
+#load stuff
+conda activate mlperf_deepcam
+module load pytorch/v1.4.0
+export PROJ_LIB=/global/homes/t/tkurth/.conda/envs/mlperf_deepcam/share/basemap
+export PYTHONPATH=/global/homes/t/tkurth/.conda/envs/mlperf_deepcam/lib/python3.7/site-packages:${PYTHONPATH}
+
+#ranks per node
+rankspernode=1
+totalranks=$(( ${SLURM_NNODES} * ${rankspernode} ))
+
+#parameters
+run_tag="deepcam_prediction_run1-cori"
+data_dir_prefix="/global/cscratch1/sd/tkurth/data/cam5_data/All-Hist"
+output_dir="/global/cscratch1/sd/tkurth/data/cam5_runs/${run_tag}"
+
+#create files
+mkdir -p ${output_dir}
+touch ${output_dir}/train.out
+
+#run training
+srun -u -N ${SLURM_NNODES} -n ${totalranks} -c $(( 256 / ${rankspernode} )) --cpu_bind=cores \
+     python train_hdf5_ddp.py \
+     --wireup_method "mpi" \
+     --run_tag ${run_tag} \
+     --data_dir_prefix ${data_dir_prefix} \
+     --output_dir ${output_dir} \
+     --max_inter_threads 0 \
+     --model_prefix "classifier" \
+     --optimizer "AdamW" \
+     --start_lr 1e-3 \
+     --num_warmup_steps 5 \
+     --num_profile_steps 1 \
+     --profile "Forward" \
+     --lr_schedule type="multistep",milestones="15000 25000",decay_rate="0.1" \
+     --lr_warmup_steps 0 \
+     --lr_warmup_factor $(( ${SLURM_NNODES} / 8 )) \
+     --weight_decay 1e-2 \
+     --amp_opt_level O1 \
+     --local_batch_size 2 |& tee -a ${output_dir}/profile.out
